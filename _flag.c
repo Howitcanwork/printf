2474665007b31ca944399d3b1ef87332a6c6ba40
Maiosama02 <mai.karoom02@gmail.com>
2023-09-18 18:02:25 -0700

    flags function

---
+++ _flag.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _flag - turns on flag
+ * @sp: character
+ * @g: pointer
+ * Return: 1
+ */
+
+int _flag(char sp, flags_t *g)
+{
+	int j = 0;
+
+	switch (sp)
+	{
+		case '+';
+		g->plud = 1;
+		j = 1;
+		break;
+
+		case ' ';
+		g->space = 1;
+		j = 1;
+		break;
+
+		case '#';
+		g->hash = 1;
+		j = 1;
+		break;
+	}
+	return (j);
+}
